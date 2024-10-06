587d211035e39e4ee6459abac69937aca3e8c8ea
Joseph <nguyenxuankien3132004@gmail.com>
2024-10-06 21:40:32 +1100

    ESTOP Update main.m

--- main.m
+++ main.m
@@ -11,20 +11,25 @@ view([135, 30]);
 axis([-1.5 1 -0.6 1.5 -0.2 1.5]);
 hold on;
 global placed;
-placed = 0;
-global row;
-global col;
+placed = 1;
+global clickedX;
+global clickedY;
+global estop;
 posPlayer = eye(4); posBot = eye(4);
 posBot(1:3, 4) = [0.5, 0.6, 0];
 posPlayer(1:3, 4) = [-1, 0.6, 0];
-
-% initial bot pos x = 0 y = 0.8 z = 0.3
+global bot;
+global player;
+% initial bot end effector pos x = 0 y = 0.8 z = 0.3
 bot = CRX10IA(posBot);
 
-%initial player pos x = -0.8 y = 0.8 z = 0.3
+%initial player end effector pos x = -0.8 y = 0.8 z = 0.3
 player = UR3e(posPlayer);
 GUI;
 
+% fig2 = figure('Position', [100, 100, 600, 480], 'MenuBar', 'none', ...
+%              'Name', 'Control', 'NumberTitle', 'off');
+% 
 initialMark = [4, 3];
 boardGame = board();
 drawX(9 - initialMark(1, 2) - 0.5,9 - initialMark(1, 1) - 0.5);
@@ -34,41 +39,99 @@ boardGame.placeMark(initialMark(1, 1), 9-initialMark(1, 2), 1);
 bot.MoveRobot(bot.homePos);
 player.MoveRobot(player.homePos);
 
+bot.currentPoint = 1;
+player.currentPoint = 1;
+
 bot.pickAndPlace([initialMark(2)-1, initialMark(1)-1]);
 
+bot.currentPoint = 1;
+player.currentPoint = 1;
+
+placed = 0;
+state = 1;
 while true
     drawnow;
+
+    if (estop == true)
+        continue;
+    end
     if placed == 1
-        boardGame.placeMark(9-row, col, 2);
-        f = parfeval(@boardGame.getBestMove, 1, 4);
-
-        if (8 - col > 1 || (8-col == 1 && (8 - row == 0 || 8 - row == 7)))
-            player.fetchObjToTransfer();
-            bot.receiveTransferAndPlace(player, [8-col 8-row]);
-        else
-            player.pickAndPlace([8-col 8-row]);
+        
+        if (state == 1)
+            boardGame.placeMark(9-clickedX, clickedY, 2);
+            f = parfeval(@boardGame.getBestMove, 1, 4);
+            state = state + 1;
         end
+        
+        % Only one of the statements below will run, therefore the state in
+        % the first statement won't likely affect the second one.
+        if (8 - clickedY > 1 || (8-clickedY == 1 && (8 - clickedX == 0 || 8 - clickedX == 7)))
+           
+            if state == 2
+                player.fetchObjToTransfer();
+                if (estop == true) continue; end
+                state = state + 1;
+            end
+            if state == 3
+                
+                bot.receiveTransferAndPlace(player, [8-clickedY 8-clickedX]);
+                if (estop == true) continue; end
+            end
 
-        drawnow;
-        placed = 0;
-        if (placed == 0)
+        else
+            if state == 2
+                player.pickAndPlace([8-clickedY 8-clickedX]);
+                if (estop == true) continue; end
+                state = state + 1;
+            end
+        end
+     
+        if (estop == true) continue; end
+
+        if state == 3
+            drawnow;
             next = fetchOutputs(f);
             boardGame.placeMark(next(1,1), next(1,2), 1);
             drawX(next(1,2) - 0.5, 9 - next(1,1) - 0.5);
             posX = 8 - next(1,2); posY = next(1,1) - 1;
-            if (posX == 1)
+            state = state + 1;
+        end
+
+        if (estop == true) continue; end
+
+        if (posX == 0)
+            if state == 4
                 player.MoveRobot([-0.5, 0.6, 0.5], [90, 90, 90]);
+                if (estop == true) continue; end
+                player.currentPoint = 1;
+                state = state + 1;
+            end
+            if state == 5
                 bot.fetchObjToTransfer();
+                if (estop == true) continue; end
+                state = state + 1;
+            end
+            if state == 6
                 player.receiveTransferAndPlace(bot, [posX, posY]);
-            else
+                if (estop == true) continue; end
+            end
+
+        else
+            if state == 4
                 bot.pickAndPlace([posX, posY]);
+                if (estop == true) continue; end
             end
         end
+
+         state = 1;
+         placed = 0;
     end
 end
 
 
 function drawX(xCenter, yCenter)
+    global fig;
+    figure(fig);
     hold on;
     lineLength = 0.3; % You can adjust this as needed
 
@@ -77,7 +140,7 @@ function drawX(xCenter, yCenter)
          [yCenter - lineLength, yCenter + lineLength], 'Color', 'r', 'LineWidth', 2);
     line([xCenter - lineLength, xCenter + lineLength], ...
          [yCenter + lineLength, yCenter - lineLength], 'Color', 'r', 'LineWidth', 2);
-
+    hold off;
 end
 
 
